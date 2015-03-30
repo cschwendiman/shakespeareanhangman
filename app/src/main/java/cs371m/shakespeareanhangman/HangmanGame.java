20179cf2b929be34af91892323b1103a4e85aefc
kat25 <kat25@cs.utexas.edu>
2015-03-30 15:37:31 -0500

    Tweaked checkGameStatus function

--- app/src/main/java/cs371m/shakespeareanhangman/HangmanGame.java
+++ app/src/main/java/cs371m/shakespeareanhangman/HangmanGame.java
@@ -1,8 +1,11 @@
 package cs371m.shakespeareanhangman;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.util.Log;
 
-import junit.framework.Assert;
+import java.io.InputStream;
+
 
 /**
  * Created by kat25 on 3/25/15.
@@ -12,6 +15,8 @@ public class HangmanGame {
     private static final String TAG = "HangmanGame";
     private static final char HIDDEN_LETTER = '_';
     private static final int MAX_WRONG_GUESSES = 6;
+
+    public enum Status {PLAYER_WIN, PLAYER_LOSS, ONGOING};
 
     // Game difficulty level
     private int difficultyLevel;
@@ -32,10 +37,16 @@ public class HangmanGame {
         // Set number of wrong guesses so far
         wrongGuesses = 0;
         // Get secret phrase for this game
+        //setupPhrases(difficultyLevel);
         secretPhrase = generateSecretPhrase(difficultyLevel);
         // Get the phrase with unrevealed characters blanked out
         currentPhrase = generateHiddenPhrase(secretPhrase);
     }
+
+/*    private void setupPhrases() {
+        Resources r = getResources();
+       InputStream quotes = r.openRawResource(R.raw.easyquotes);
+    }*/
 
     // Chooses a secret phrase for the game based on the difficulty level
     private String generateSecretPhrase(int difficultyLevel) {
@@ -113,12 +124,58 @@ public class HangmanGame {
 
     }
 
-    public boolean checkForWin() {
+    /*
+     * Says whether the player won, the player lost, or the game is still ongoing
+     */
+    public Status checkGameStatus () {
+        if(checkForWin()) {
+            return Status.PLAYER_WIN;
+        } else if(checkForLoss()) {
+            return Status.PLAYER_LOSS;
+        } else {
+            return Status.ONGOING;
+        }
+    }
+
+    private boolean checkForWin() {
         return secretPhrase.equals(currentPhrase);
     }
 
-    public boolean checkForLoss() {
+    private boolean checkForLoss() {
         return wrongGuesses == MAX_WRONG_GUESSES;
     }
 
+
+/*    private class Phrase {
+        private String quote;
+        private String play;
+        private int act;
+        private int scene;
+
+        public Phrase(String quote, String play, int act, int scene) {
+            this.quote = quote;
+            this.play = play;
+            this.act = act;
+            this.scene = scene;
+        }
+
+        public String getQuote() {
+            return quote;
+        }
+
+        public String getPlay() {
+            return play;
+        }
+
+        public int getAct() {
+            return act;
+        }
+
+        public int getScene() {
+            return scene;
+        }
+
+    }*/
 }
+
+
