e30bf79b1eafa511c84b4e884b3cb183166b9522
Cassie Schwendiman <cassie.schwendiman@gmail.com>
2015-05-08 14:45:17 -0500

    add carousel to screenshots section

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+$(document).ready(function(){
+      $('#screenshots-carousel').slick({
+      		infinite: true,
+  			slidesToShow: 3,
+  			slidesToScroll: 1,
+  			autoplaySpeed: 2000
+      });
+    });
